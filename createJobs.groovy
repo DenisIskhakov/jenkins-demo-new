702fa77424ce2289763ee75194e191112a3e4b7e
DIskhakov <iskhakov.denis@mail.ru>
2022-05-05 16:18:57 +0300

    jenkins

--- createJobs.groovy
+++ createJobs.groovy
@@ -12,7 +12,7 @@ pipelineJob('theme-park-job') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                        url 'https://github.com/DenisIskhakov/spring-boot-api-example-1.git'
                     }
                     branch 'master'
                 }
